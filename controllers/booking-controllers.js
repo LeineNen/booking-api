2c309f4f630779a484ed8fc7d3308aa1075d5981
Mary Mag <maymag4on@gmail.com>
2024-10-03 15:18:35 +0000

    postmanworked

--- controllers/booking-controllers.js
+++ controllers/booking-controllers.js
@@ -2,50 +2,42 @@
 import { booking } from "../models/booking-model.js"
 
 
-export const getAllBookings = (req, res, next) => {
-    try {
+export const getAllBookings = (req, res) => {
+    
         res.status(200).json('These are all bookings')
-    } catch (error) {next(error);
-        
     }
-}
+        
+    
 
-export const getOneBooking = (req, res, next) => {
-    try {
+export const getOneBooking = (req, res) => {
+
         res.status(200).json('This is one booking')
-    } catch (error) {next(error);
+    } 
         
-    }
-}
 
 
 
-export const postBooking = async (req, res, next) => {
-    try {
-        const newBooking = new booking()
-        const bookings = await newBooking.save(req.body)
+export const postBooking = async (req, res) => {
+    
+        const newBooking = new booking(req.body)
+        const bookings = await newBooking.save()
     
         // const booking = newBooking
     
         res.status(201).json(bookings)
-    } catch (error) {next(error);
-        
-    }
-}
+    } 
 
 
 export const updateBooking = (req, res) => {
-   try {
+
      res.status(200).json('Booking has been updated')
-   } catch (error) {next(error);
-    
-   }
-}
+   } 
+
 
 export const deleteBooking = (req, res) => {
-    try {
+
         res.status(200).json('Booking has been deleted')
-    } catch (error) {next(error);
+    } 
         
-    }
-}
+    
+
