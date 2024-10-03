95443ce09cbf0e1930da79eca05dbc80f2d7b1a3
Mary Mag <maymag4on@gmail.com>
2024-10-03 15:22:58 +0000

    added cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import express from 'express'
 import { bookingRouter } from './routes/all-routes.js'
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import cors from 'cors'
 
 
 // connect to mongodb database
@@ -14,9 +15,11 @@ const app = express()
 const PORT = 3001
 
 // use middlewares
+app.use(cors())
 app.use(express.json())
-
 app.use(bookingRouter)
+
+
 
 
 app.listen(PORT, () => console.log(`server is listening on ${PORT}`))
