75f2ffce13f4ee10c1053b4db544499c73cc8984
Mary Mag <maymag4on@gmail.com>
2024-10-04 14:07:36 +0000

    Added the variable bookings not to hard code

--- controllers/booking-controllers.js
+++ controllers/booking-controllers.js
@@ -2,42 +2,48 @@
 import { booking } from "../models/booking-model.js"
 
 
-export const getAllBookings = (req, res) => {
-    
-        res.status(200).json('These are all bookings')
+export const getAllBookings = async (req, res, next) => {
+
+    try {
+        const bookings = await booking.find();
+        res.status(200).json(bookings);
+    } catch (error) {
+        next(error);
+
     }
-        
-    
+}
+
+
 
 export const getOneBooking = (req, res) => {
 
-        res.status(200).json('This is one booking')
-    } 
-        
+    res.status(200).json('This is one booking')
+}
+
 
 
 
 export const postBooking = async (req, res) => {
-    
-        const newBooking = new booking(req.body)
-        const bookings = await newBooking.save()
-    
-        // const booking = newBooking
-    
-        res.status(201).json(bookings)
-    } 
+
+    const newBooking = new booking(req.body)
+    const bookings = await newBooking.save()
+
+    // const booking = newBooking
+
+    res.status(201).json(bookings)
+}
 
 
 export const updateBooking = (req, res) => {
 
-     res.status(200).json('Booking has been updated')
-   } 
+    res.status(200).json('Booking has been updated')
+}
 
 
 export const deleteBooking = (req, res) => {
 
-        res.status(200).json('Booking has been deleted')
-    } 
-        
-    
+    res.status(200).json('Booking has been deleted')
+}
+
+
 
